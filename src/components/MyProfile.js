3f56ae4feb0f4c728fd42e5d52bcb3084c28e485
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-12 16:24:56 +0800

    [ZhangJin] feat: form finish

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,9 +2,35 @@ import React, {Component} from 'react';
 import './myProfile.less';
 
 class MyProfile extends Component {
+  state={
+    name:'',
+    gender:'',
+    description:''
+  }
+  change=function(e,inputid){
+    this.setState({[inputid]:e.target.value});
+  }
+
+ 
+
   render() {
     return (
-      <form></form>
+      <form>
+        <h1>My Profile</h1>
+        <div class="form-group">
+          <label htmlFor="name">Name:</label>
+          <input className="form-control" id="name" value={this.state.name} onChange={(e) =>this.change(e,"name")}></input>
+        </div>
+        <div class="form-group">
+          <label htmlFor="gender">Gender:</label>
+          <input className="form-control" id="gender" value={this.state.gender} onChange={(e) =>this.change(e,"gender")}></input>
+        </div>
+        <div class="form-group">
+          <label htmlFor="description">Description</label>
+          <input className="form-control" id="description" value={this.state.description} onChange={(e) =>this.change(e,"description")}></input>
+        </div>
+        <button type="button" disabled={(this.state.name===''||this.state.gender===''||this.state.description==='')} className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
